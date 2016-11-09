c141add2240b14ce142b2c34b3f57a893fb7195e
Richard Polzin <polzin.richard@gmail.com>
2016-11-09 11:54:08 +0100

    added graph.py for plotting

---
+++ simple_neural_network/graph.py
@@ -0,0 +1,32 @@
+import main
+from bokeh.layouts import gridplot
+from bokeh.palettes import Spectral10
+from bokeh.plotting import figure, output_file, show
+
+if __name__ == '__main__':
+    output_file = ('plots.html')
+
+    # Random Weights
+
+    y = [main.neural_net(1000, 0.5, True, False ) for i in range(10)]
+    x = [i for i in range(1000)]
+
+    p1 = figure(title='Influence of Random weight', x_axis_label='Learning iterations over the whole sample', y_axis_label='Sum of squared deviation from desired output')
+
+    for n, i in enumerate(y):
+        p1.line(x, i, legend = 'Random Weight '+str(n), line_width = 2, line_color=Spectral10[n])
+
+    # Shuffle Input
+
+    y = [main.neural_net(1000, 0.5, False, True) for i in range(10)]
+    x = [i for i in range(1000)]
+
+    p2 = figure(title='Influence of shuffled learning examples', x_axis_label='Learning iterations over the whole sample', y_axis_label='Sum of squared deviation from desired output')
+
+    for n, i in enumerate(y):
+        if n == 0:
+            p2.line(x, main.neural_net(1000, 0.5, False, False), legend = 'No Shuffle', line_width = 2, line_color=Spectral10[n])
+        else:
+            p2.line(x, i, legend = 'Shuffled Input '+str(n-1), line_width = 2, line_color=Spectral10[n])
+
+    show(gridplot([p1, p2], ncols = 2, plot_width = 600, plot_height = 450))
--- simple_neural_network/main.py
+++ simple_neural_network/main.py
@@ -4,29 +4,30 @@ import logging
 import random
 import itertools
 
-if __name__ == "__main__":
-    np.set_printoptions(formatter={'float_kind': lambda x: "%.3f" % x})
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger(__name__)
-    logger.info("Program started")
+np.set_printoptions(formatter={'float_kind': lambda x: "%.3f" % x})
+logging.basicConfig(level=logging.ERROR)
+logger = logging.getLogger(__name__)
+logger.info("Program started")
 
+def neural_net(iterations, learning_rate, random_weights = True, shuffle_samples = True):
     learning_examples = [l for l in util.unique(itertools.permutations('00000001'))]
     learning_examples = np.array(learning_examples, dtype=int)
     logger.debug("Learning Examples : \n{}".format(learning_examples))
 
-    np.random.seed(0)
+    if not random_weights:
+        np.random.seed(0)
     w1 = np.random.uniform(-1, 1, (3, 8))
     b1 = np.random.uniform(-1, 1, (3, 1))
     w2 = np.random.uniform(-1, 1, (8, 3))
     b2 = np.random.uniform(-1, 1, (8, 1))
 
-    iterations = 13000
-    learning_rate = 0.3
+    errors = []
 
     for i in range(iterations):
         error_sum = 0
         index=list(range(8))
-        np.random.shuffle(index)
+        if shuffle_samples:
+            np.random.shuffle(index)
         for j in index:
             a1 = np.array([learning_examples[j]]).T
             # logger.debug("Layer 1 Activation : \n{}".format(a1))
@@ -50,30 +51,33 @@ if __name__ == "__main__":
 
             w2delta = learning_rate * (np.outer(d3, a2))
             w2 += w2delta
-            logger.debug("Weights 2: \n{}".format(w2))
+            # logger.debug("Weights 2: \n{}".format(w2))
             # logger.debug("Weights 2 Delta: \n{}".format(w2delta))
 
             w1delta = learning_rate * (np.outer(d2, a1))
             w1 += w1delta
-            logger.debug("Weights 1: \n{}".format(w1))
+            # logger.debug("Weights 1: \n{}".format(w1))
             # logger.debug("Weights 1 Delta: \n{}".format(w1delta))
 
             b2 += learning_rate * d3
-            logger.debug("Layer 2 Bias : \n{}".format(b2))
+            # logger.debug("Layer 2 Bias : \n{}".format(b2))
             # logger.debug("Layer 2 Bias Delta : \n{}".format(learning_rate * d3))
 
             b1 += learning_rate * d2
-            logger.debug("Layer 1 Bias : \n{}".format(b1))
+            # logger.debug("Layer 1 Bias : \n{}".format(b1))
             # logger.debug("Layer 2 Bias Delta : \n{}".format(learning_rate * d2))
 
             error_sum += abs(np.sum(np.absolute(d3)))
+        errors.append(error_sum)
         if i % 100 == 0: 
           logger.info("Overall error after {} iterations : {}".format(i, error_sum))
 
     logger.info("Final error after {} iterations : {}".format(iterations, error_sum))
+    return errors
 
 
-
+if __name__ == '__main__':
+    neural_net(1000, 0.3)
 
 
 
